7bbe8027d9d8b07433d70db30170f1f9b11d267c
asifikbal09 <asif.ikbal662@gmail.com>
2023-12-19 00:37:30 +0600

    [DONE] Review routes added.

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,18 +1,23 @@
-import { Router } from "express";
-import { CourseRoutes } from "../modules/course/course.route";
-import { CategoryRoutes } from "../modules/category/category.route";
+import { Router } from 'express';
+import { CourseRoutes } from '../modules/course/course.route';
+import { CategoryRoutes } from '../modules/category/category.route';
+import { ReviewRoutes } from '../modules/review/review.route';
 
 const router = Router();
 
 const moduleRoutes = [
- {
-    path:'/course',
-    route:CourseRoutes
- },
- {
-    path:'/categories',
-    route:CategoryRoutes
- },
+  {
+    path: '/course',
+    route: CourseRoutes,
+  },
+  {
+    path: '/categories',
+    route: CategoryRoutes,
+  },
+  {
+    path: '/reviews',
+    route: ReviewRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
